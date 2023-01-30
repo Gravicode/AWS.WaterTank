fed34e949f9ae52546989774e10ca349eb4b7ed7
muhammad ibnu fadhil <mifmasterz@gmail.com>
2023-01-30 08:32:27 +0700

    init

---
+++ src/AWS.WaterTank/AWS.Consumer/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace AWS.Consumer;
+class Program
+{
+    private static ManualResetEvent manualResetEvent;
+
+    static void Main(string[] args)
+    {
+        var iotEndPoint = "a2teks7xu15e4c-ats.iot.ap-southeast-1.amazonaws.com";
+        var iotPort = 8883;
+
+        Console.WriteLine("AWS IoT dotnetcore message consumer starting..");
+        var rootCert = Path.Join(AppContext.BaseDirectory, "AmazonRootCA1.crt");
+        var clientcert = Path.Join(AppContext.BaseDirectory, "certificate.cert.pfx");
+        var privatecert = Path.Join(AppContext.BaseDirectory, "water-monitor-gateway.private.key");
+        var caCertSource = File.ReadAllBytes(rootCert);//UTF8Encoding.UTF8.GetBytes("Need AWS root CA certificate");
+        var clientCertSource = File.ReadAllBytes(clientcert); //UTF8Encoding.UTF8.GetBytes("Need your AWS client CA certificate");
+        var privateKeyData = File.ReadAllBytes(privatecert);//UTF8Encoding.UTF8.GetBytes("Need your AWS private key");
+        X509Certificate CaCert = new X509Certificate(caCertSource);
+        X509Certificate ClientCert = new X509Certificate2(clientCertSource, "123qweasd");
+
+        var client = new MqttClient(iotEndPoint, iotPort, true, CaCert, ClientCert, MqttSslProtocols.TLSv1_2);
+
+
+        client.MqttMsgSubscribed += IotClient_MqttMsgSubscribed;
+        client.MqttMsgPublishReceived += IotClient_MqttMsgPublishReceived;
+
+        string clientId = Guid.NewGuid().ToString();
+        client.Connect(clientId);
+        Console.WriteLine($"Connected to AWS IoT with client ID: {clientId}");
+
+        string topic = "Hello/World";
+        Console.WriteLine($"Subscribing to topic: {topic}");
+        client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+        // Keep the main thread alive for the event receivers to get invoked
+        KeepConsoleAppRunning(() =>
+        {
+            client.Disconnect();
+            Console.WriteLine("Disconnecting client..");
+        });
+    }
+
+    private static void IotClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        Console.WriteLine("Message received: " + Encoding.UTF8.GetString(e.Message));
+    }
+
+    private static void IotClient_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+    {
+        Console.WriteLine($"Successfully subscribed to the AWS IoT topic.");
+    }
+
+    private static void KeepConsoleAppRunning(Action onShutdown)
+    {
+        manualResetEvent = new ManualResetEvent(false);
+        Console.WriteLine("Press CTRL + C or CTRL + Break to exit...");
+
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            onShutdown();
+            e.Cancel = true;
+            manualResetEvent.Set();
+        };
+
+        manualResetEvent.WaitOne();
+    }
+}
---
+++ src/AWS.WaterTank/AWS.Device/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace AWS.Device;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var iotEndPoint = "a2teks7xu15e4c-ats.iot.ap-southeast-1.amazonaws.com";
+        var iotPort = 8883;
+        var deviceId = "water-monitor-gateway";
+
+        var topicShadowUpdate = string.Format("$aws/things/{0}/shadow/update", deviceId);
+        var topicShadowGet = string.Format("$aws/things/{0}/shadow/get", deviceId);
+
+        var message = "{\"state\":{\"desired\":{\"My message\":\"Hello World\"}}}";
+        var rootCert = Path.Join(AppContext.BaseDirectory, "AmazonRootCA1.crt");
+        var clientcert = Path.Join(AppContext.BaseDirectory, "certificate.cert.pfx");
+        var privatecert = Path.Join(AppContext.BaseDirectory, "water-monitor-gateway.private.key");
+        var caCertSource = File.ReadAllBytes(rootCert);//UTF8Encoding.UTF8.GetBytes("Need AWS root CA certificate");
+        var clientCertSource = File.ReadAllBytes(clientcert); //UTF8Encoding.UTF8.GetBytes("Need your AWS client CA certificate");
+        var privateKeyData = File.ReadAllBytes(privatecert);//UTF8Encoding.UTF8.GetBytes("Need your AWS private key");
+        X509Certificate CaCert = new X509Certificate(caCertSource);
+        X509Certificate ClientCert = new X509Certificate2(clientCertSource,"123qweasd");
+
+        var iotClient = new MqttClient(iotEndPoint, iotPort, true, CaCert, ClientCert, MqttSslProtocols.TLSv1_2);
+        //string clientId = Guid.NewGuid().ToString();
+        var connectCode = iotClient.Connect(deviceId);
+        Console.WriteLine($"Connected to AWS IoT with client id: {deviceId}.");
+
+        /*
+        var clientSetting = new MqttClientSetting
+        {
+            BrokerName = iotEndPoint,
+            BrokerPort = iotPort,
+            CaCertificate = CaCert,
+            ClientCertificate = ClientCert,
+            SslProtocol = System.Security.Authentication.SslProtocols.Tls12
+        };
+
+        var iotClient = new Mqtt(clientSetting);
+        */
+        iotClient.MqttMsgPublishReceived += (p1,p2) =>
+        {
+            Debug.WriteLine("Received message: " + Encoding.UTF8.GetString(p2.Message));
+        };
+
+        iotClient.MqttMsgSubscribed += (a, b) => { Debug.WriteLine("Subscribed"); };
+
+        Debug.WriteLine("Connecting....");
+
+
+
+        //var connectCode = iotClient.Connect(connectSetting);
+        string topic = "Hello/World";
+        ushort packetId = 1;
+
+        iotClient.Subscribe(new string[] { topicShadowGet, topic }, new byte[]
+            { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE,MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+        iotClient.Publish(topicShadowUpdate, Encoding.UTF8.GetBytes(message),
+             MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE , false);
+        int i = 0;
+        message = "Test message";
+        while (true)
+        {
+            iotClient.Publish(topic, Encoding.UTF8.GetBytes($"{message} {i}"));
+            Console.WriteLine($"Published: {message} {i}");
+            i++;
+            Thread.Sleep(5000);
+        }
+
+    }
+}
+
